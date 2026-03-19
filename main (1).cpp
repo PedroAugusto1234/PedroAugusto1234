44da64a792aa6b780f32502cf6094db56b5af87b
PedroAugusto1234 <127551531+PedroAugusto1234@users.noreply.github.com>
2026-03-19 14:52:43 -0300

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    double n1,n2,n3,n4;
+    
+    cout<<"Calculadora de Aprovação";
+    cout<<"\n ------------------------------------------";
+    
+    cout<<"\n digite nota B1";
+    cin>> n1;
+    
+    cout<<"\n digite nota B2";
+    cin>> n2;
+    
+    cout<<"\n digite nota B3";
+    cin>> n3;
+    
+    n4 = (n1 + n2 + n3)/3;
+    
+    cout<<"\n media="<<n4;
+    
+    if (n4 >= 70){
+        cout<<"\n Você foi aprovado";
+        
+    }else{
+    
+    cout<<"\n Você foi reprovado";
+    }
+    
+    
+    return 0;
+}
